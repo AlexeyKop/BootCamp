26ff6a8768b7b16e102cd54012856cfe1f18d658
AlexeyKop <a.kop@bk.ru>
2023-12-26 01:11:17 +0400

    first steps in Flask framework

--- Les 09_Flask_BootCamp/templates/main.py
+++ Les 09_Flask_BootCamp/templates/main.py
@@ -3,16 +3,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "<h1>Hello, world!</h1>"
 
+'''
 @app.route("/")
 def index():
     return render_template('index.html')
 
+'''
 
 @app.route("/info")
 def info():
     return "<h1>GEEKBRAINS</h1>"
 
 
-if __name___ == '__main__':
+
+if __name__ == '__main__':
     app.run()
